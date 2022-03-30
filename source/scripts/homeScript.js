742b4354823bb58aec5ff06025c2f488ef62ccd6
elo0501 <eloisemercado@gmail.com>
2022-03-30 15:51:56 +0200

    Affichage de nos messages à droite + les messages restent dans la chatbox quand on resize la fenêtre

--- source/scripts/homeScript.js
+++ source/scripts/homeScript.js
@@ -1,33 +1,33 @@
 class Contact {
-  constructor(name, lastMessage, messageHour) {
-    this.name = name;
-    this.lastMessage = lastMessage;
-    this.messageHour = messageHour;
-  }
+    constructor(name, lastMessage, messageHour) {
+        this.name = name;
+        this.lastMessage = lastMessage;
+        this.messageHour = messageHour;
+    }
 }
 
 class Message {
-  constructor(author, content, time) {
-    this.author = author;
-    this.content = content;
-    this.time = time;
-  }
+    constructor(author, content, time) {
+        this.author = author;
+        this.content = content;
+        this.time = time;
+    }
 }
 
 async function getUsername() {
-  try {
-    const res = await fetch('/getUsername', {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' }
-    })
-    const data = await res.json()
-    if(res.status === 400 || res.status === 401){
-      return display.textContent = `${data.message}. ${data.error ? data.error : ''}`
+    try {
+        const res = await fetch('/getUsername', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        const data = await res.json()
+        if (res.status === 400 || res.status === 401) {
+            return display.textContent = `${data.message}. ${data.error ? data.error : ''}`
+        }
+        return data.username;
+    } catch (err) {
+        console.log(err.message)
     }
-    return data.username;
-  } catch (err) {
-      console.log(err.message)
-  }
 }
 
 
@@ -36,7 +36,7 @@ var myPseudo = "Random";
 var ID = -1;
 
 username.then(function(result) {
-  myPseudo = result;
+    myPseudo = result;
 })
 
 
@@ -58,118 +58,117 @@ var messageNumber = 0;
 
 // Que faire lorsque la connexion est établie
 ws.addEventListener("open", () => {
-  console.log("We are connected");
+    console.log("We are connected");
 });
 
 
 // Que faire quand le client reçoit un message du serveur
 ws.addEventListener("message", data => {
-  if (messageNumber == 0) { // Le tout premier message est celui du serveur qui communique l'ID du client
-    console.log(data.data);
-    ID = parseInt(data.data);
-    alert(`Your ID is ${ID}`);
-  }
-  else {
-    let msg = JSON.parse(data.data);
-    console.log(msg);
-    let message = new Message(msg.author, msg.content, msg.time);
-    messagesArray.push(message);
-    renderMessages();
-  }
-  messageNumber++;
+    if (messageNumber == 0) { // Le tout premier message est celui du serveur qui communique l'ID du client
+        console.log(data.data);
+        ID = parseInt(data.data);
+        alert(`Your ID is ${ID}`);
+    } else {
+        let msg = JSON.parse(data.data);
+        console.log(msg);
+        let message = new Message(msg.author, msg.content, msg.time);
+        messagesArray.push(message);
+        renderMessages();
+    }
+    messageNumber++;
 });
 
 
 function getTime() {
-  let date = new Date();
-  let milisec = Date.now();
-  let seconds = milisec / 1000;
-  let minutes = seconds / 60;
-  minutes -= date.getTimezoneOffset(); // Permet de ne pas appliquer le fuseau horaire du client
-  let hours = minutes / 60;
-  let result = Math.floor(hours % 24) + ":" + Math.floor(minutes % 60);
-  return result;
+    let date = new Date();
+    let milisec = Date.now();
+    let seconds = milisec / 1000;
+    let minutes = seconds / 60;
+    minutes -= date.getTimezoneOffset(); // Permet de ne pas appliquer le fuseau horaire du client
+    let hours = minutes / 60;
+    let result = Math.floor(hours % 24) + ":" + Math.floor(minutes % 60);
+    return result;
 }
 
 
 function renderContacts() {
-  let div = document.getElementById("contact-list");
-  div.innerHTML = "";
-  for (let i = 0; i < contact_list.length; i++) {
-    let contact = document.createElement("div");
-    contact.classList.add("contact");
-    contact.id = "contact-" + i;
-    div.appendChild(contact);
+    let div = document.getElementById("contact-list");
+    div.innerHTML = "";
+    for (let i = 0; i < contact_list.length; i++) {
+        let contact = document.createElement("div");
+        contact.classList.add("contact");
+        contact.id = "contact-" + i;
+        div.appendChild(contact);
 
-    let grid80 = document.createElement("div");
-    grid80.classList.add("grid-80-20");
-    contact.appendChild(grid80);
+        let grid80 = document.createElement("div");
+        grid80.classList.add("grid-80-20");
+        contact.appendChild(grid80);
 
-    let contact_title = document.createElement("p");
-    contact_title.classList.add("contact-title");
-    contact_title.innerText = contact_list[i].name;
-    grid80.appendChild(contact_title);
+        let contact_title = document.createElement("p");
+        contact_title.classList.add("contact-title");
+        contact_title.innerText = contact_list[i].name;
+        grid80.appendChild(contact_title);
 
-    let message_hour = document.createElement("p");
-    message_hour.classList.add("message-hour");
-    message_hour.innerText = contact_list[i].messageHour;
-    grid80.appendChild(message_hour);
+        let message_hour = document.createElement("p");
+        message_hour.classList.add("message-hour");
+        message_hour.innerText = contact_list[i].messageHour;
+        grid80.appendChild(message_hour);
 
-    let last_message = document.createElement("p");
-    last_message.classList.add("last-message");
-    last_message.innerText = contact_list[i].lastMessage;
-    contact.appendChild(last_message);
-  }
+        let last_message = document.createElement("p");
+        last_message.classList.add("last-message");
+        last_message.innerText = contact_list[i].lastMessage;
+        contact.appendChild(last_message);
+    }
 }
 
 renderContacts();
 
-var selectContact = function (e) {
-  for (let i = 0; i < contact_list.length; i++) {
-    let contact = document.getElementById("contact-" + i);
-    contact.classList.remove("selected");
-  }
-  e.currentTarget.classList.add("selected");
+var selectContact = function(e) {
+    for (let i = 0; i < contact_list.length; i++) {
+        let contact = document.getElementById("contact-" + i);
+        contact.classList.remove("selected");
+    }
+    e.currentTarget.classList.add("selected");
 
-  // Affichage de la discussion sur la partie droite en cachant l'accueil
-  let activeAccueil = document.querySelector("#accueil");
-  activeAccueil.classList.add("hidden");
-  let footer = document.querySelector("#footer");
-  footer.classList.add("hidden");
-  let chatHome = document.querySelector("#chat");
-  chatHome.classList.remove("hidden");
+    // Affichage de la discussion sur la partie droite en cachant l'accueil
+    let activeAccueil = document.querySelector("#accueil");
+    activeAccueil.classList.add("hidden");
+    let footer = document.querySelector("#footer");
+    footer.classList.add("hidden");
+    let chatHome = document.querySelector("#chat");
+    chatHome.classList.remove("hidden");
 
-  // Afficher les messages
-  renderMessages();
+    // Afficher les messages
+    renderMessages();
 };
 
 // Ajout des évènements au clic sur contact
 for (let i = 0; i < contact_list.length; i++) {
-  let contact = document.querySelector("#contact-" + i);
-  contact.addEventListener("click", selectContact, true);
+    let contact = document.querySelector("#contact-" + i);
+    contact.addEventListener("click", selectContact, true);
 }
 
 // TODO : ajouter un bouton retour pour réafficher l'accueil
 
 
 function sendMessage() {
-  let chatbox = document.getElementById("chat-box");
-  if (chatbox.value.length == 0) {
-    console.log("Aucun message à envoyer");
-    return;
-  }
+    let chatbox = document.getElementById("chat-box");
+    if (chatbox.value.length == 0) {
+        console.log("Aucun message à envoyer");
+        return;
+    }
 
-  let message = new Message(myPseudo, chatbox.value, getTime());
-  ws.send(JSON.stringify(message));
+    let message = new Message(myPseudo, chatbox.value, getTime());
+    ws.send(JSON.stringify(message));
 
-  chatbox.value = "";
+    chatbox.value = "";
 }
 
 // Touche entrée liée au bouton d'envoi de message
-window.addEventListener('keyup', function (event) {
-  if (event.keyCode === 13) {
-    this.document.getElementById("send").click();
-  }
+window.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        this.document.getElementById("send").click();
+    }
 });
 
 
@@ -183,78 +182,69 @@ window.addEventListener('keyup', function (event) {
 <p class="response-time time"> 15h04</p>  
 */
 function renderMessages() {
-  let messagesChat = document.getElementById("messages-chat");
-  messagesChat.innerHTML = "";
-
-  for (let i = 0; i < messagesArray.length; i++) {
-    var author = messagesArray[i].author;
-
-    // Check si premier message pour ajouter le nom
-    if (i == 0 || (i > 0 && messagesArray[i - 1].author != author)) {
-      let newMsgDiv = document.createElement("div");
-      newMsgDiv.classList.add("message");
-      messagesChat.appendChild(newMsgDiv);
-      let text = document.createElement("p");
-      text.classList.add("username");
-      text.appendChild(document.createTextNode(author));
-      newMsgDiv.appendChild(text);
+    let messagesChat = document.getElementById("messages-chat");
+    messagesChat.innerHTML = "";
+    for (let i = 0; i < messagesArray.length; i++) {
+        var author = messagesArray[i].author;
+        let messageId = parseInt(author.split('#')[1]);
+        // Check si premier message pour ajouter le nom
+        if (i == 0 || (i > 0 && messagesArray[i - 1].author != author)) {
+            let newMsgDiv = document.createElement("div");
+            newMsgDiv.classList.add("message");
+            messagesChat.appendChild(newMsgDiv);
+            let text = document.createElement("p");
+            text.classList.add("username");
+            if (messageId == ID) {
+                text.classList.add("response-username");
+            }
+            text.appendChild(document.createTextNode(author));
+            newMsgDiv.appendChild(text);
+        }
+        if (messageId == ID) {
+            let newMsgDiv = document.createElement("div");
+            newMsgDiv.classList.add("message");
+            newMsgDiv.classList.add("text-only");
+            messagesChat.appendChild(newMsgDiv);
+            let response = document.createElement("div");
+            response.classList.add("response");
+            newMsgDiv.appendChild(response);
+            let text = document.createElement("p");
+            text.classList.add("text");
+            text.appendChild(document.createTextNode(messagesArray[i].content));
+            response.appendChild(text);
+        } else {
+            let newMsgDiv = document.createElement("div");
+            newMsgDiv.classList.add("message");
+            newMsgDiv.classList.add("text-only");
+            messagesChat.appendChild(newMsgDiv);
+            let text = document.createElement("p");
+            text.classList.add("text");
+            text.appendChild(document.createTextNode(messagesArray[i].content));
+            newMsgDiv.appendChild(text);
+        }
+        // Check si c'est le dernier message pour afficher l'heure
+        // ToDo: afficher l'heure si message date de + de 5mn
+        // || (i > 0 && messagesArray[i].time > new Date(messagesArray[i-1].time.getTime() + 1 * 60000) )
+        if (i == messagesArray.length - 1 || (i < messagesArray.length && messagesArray[i + 1].author != author)) {
+            let newMsgDiv = document.createElement("div");
+            newMsgDiv.classList.add("message");
+            messagesChat.appendChild(newMsgDiv);
+            let time = document.createElement("p");
+            time.classList.add("time");
+            if (messageId == ID) {
+                time.classList.add("response-time");
+            }
+            // let formatted_time = messagesArray[i].time.toLocaleTimeString().substring(0, 5);
+            // time.appendChild(document.createTextNode(formatted_time));
+            time.appendChild(document.createTextNode(messagesArray[i].time));
+            newMsgDiv.appendChild(time);
+        }
     }
-
-    let messageId = parseInt(author.split('#')[1]);
-    if (messageId == ID) {
-      let newMsgDiv = document.createElement("div");
-      newMsgDiv.classList.add("message");
-      newMsgDiv.classList.add("text-only");
-      messagesChat.appendChild(newMsgDiv);
-
-      let response = document.createElement("div");
-      response.classList.add("response");
-      newMsgDiv.appendChild(response);
-
-      let text = document.createElement("p");
-      text.classList.add("text");
-      text.appendChild(document.createTextNode(messagesArray[i].content));
-      response.appendChild(text);
-
-    }
-    else {
-      /*
-      <div class="message text-only">
-        <p class="text"> Ça va ? </p>
-      </div>
-      <p class="time"> 14h58</p> */
-      let newMsgDiv = document.createElement("div");
-      newMsgDiv.classList.add("message");
-      newMsgDiv.classList.add("text-only");
-      messagesChat.appendChild(newMsgDiv);
-
-      let text = document.createElement("p");
-      text.classList.add("text");
-      text.appendChild(document.createTextNode(messagesArray[i].content));
-      newMsgDiv.appendChild(text);
-    }
-
-    // Check si c'est le dernier message pour afficher l'heure
-    // ToDo: afficher l'heure si message date de + de 5mn
-    // || (i > 0 && messagesArray[i].time > new Date(messagesArray[i-1].time.getTime() + 1 * 60000) )
-
-    if (i == messagesArray.length - 1 || (i < messagesArray.length && messagesArray[i + 1].author != author)) {
-      let newMsgDiv = document.createElement("div");
-      newMsgDiv.classList.add("message");
-      messagesChat.appendChild(newMsgDiv);
-      let time = document.createElement("p");
-      time.classList.add("time");
-      // let formatted_time = messagesArray[i].time.toLocaleTimeString().substring(0, 5);
-      // time.appendChild(document.createTextNode(formatted_time));
-      time.appendChild(document.createTextNode(messagesArray[i].time));
-      newMsgDiv.appendChild(time);
-    }
-  }
-  updateScroll();
+    updateScroll();
 }
 
 // Maintient la scroll bar au bas à chaque message ajouté
 function updateScroll() {
-  var messagesChat = document.querySelector("#messages-chat");
-  messagesChat.scrollTop = messagesChat.scrollHeight;
+    var messagesChat = document.querySelector("#messages-chat");
+    messagesChat.scrollTop = messagesChat.scrollHeight;
 }
