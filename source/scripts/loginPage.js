0870e0d6afe5cb996bf54ad81b8e7a1ad8cea808
XAVIER Vincent <vincent.xavier@ens.uvsq.fr>
2022-04-20 22:58:06 +0200

    Implémentation du password checker

--- source/scripts/loginPage.js
+++ source/scripts/loginPage.js
@@ -1,62 +1,95 @@
+let password_input = document.getElementById("passwordSignup");
+let box1 = document.getElementById("strenght_password_box_1");
+let box2 = document.getElementById("strenght_password_box_2");
+let box3 = document.getElementById("strenght_password_box_3");
 
-var switchLoginForm = function (e) {
-  let exActive = document.querySelector(".login.active");
-  exActive.classList.remove("active");
-  exActive.classList.add("inactive");
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
 
-  e.currentTarget.classList.remove("inactive");
-  e.currentTarget.classList.add("active");
+function StrengthChecker(PasswordParameter) {
+    if (strongPassword.test(PasswordParameter)) {
+        box1.style.backgroundColor = "green";
+        box2.style.backgroundColor = "green";
+        box3.style.backgroundColor = "green";
+    } else if (mediumPassword.test(PasswordParameter)) {
+        box1.style.backgroundColor = "orange";
+        box2.style.backgroundColor = "orange";
+        box3.style.backgroundColor = "gray";
+    } else if (PasswordParameter == '') {
+        box1.style.backgroundColor = "gray";
+        box2.style.backgroundColor = "gray";
+        box3.style.backgroundColor = "gray";
+    } else {
+        box1.style.backgroundColor = "red";
+        box2.style.backgroundColor = "gray";
+        box3.style.backgroundColor = "gray";
+    }
+}
 
-  let inactive = document.querySelector(".login.inactive");
-  inactive.addEventListener("click", switchLoginForm, true);
+// Ajoute un événement sur l'input pour vérifier la rigidité du mot de passe après que l'utilisateur l'ait tapé
+password_input.addEventListener("input", () => {
+    StrengthChecker(password_input.value);
+});
 
-  // Enlever le required des formulaires
-  let usernameLogin = document.getElementById("usernameLogin");
-  if (usernameLogin.required) {
-    // Disable Login
-    document.getElementById("usernameLogin").required = false;
-    document.getElementById("passwordLogin").required = false;
-    // Enable Signup
-    document.getElementById("usernameSignup").required = true;
-    document.getElementById("emailSignup").required = true;
-    document.getElementById("passwordSignup").required = true;
-  }
-  else {
-    // Disable Signup
-    document.getElementById("usernameSignup").required = false;
-    document.getElementById("emailSignup").required = false;
-    document.getElementById("passwordSignup").required = false;
-    // Enable Login
-    document.getElementById("usernameLogin").required = true;
-    document.getElementById("passwordLogin").required = true;
-  }
+var switchLoginForm = function(e) {
+    let exActive = document.querySelector(".login.active");
+    exActive.classList.remove("active");
+    exActive.classList.add("inactive");
 
-  // Remplaçage du formulaire
-  let hiddenForm = document.querySelector(".log-form.hidden");
-  let activeForm = document.querySelector(".log-form.visible");
-  activeForm.classList.remove("visible");
-  activeForm.classList.add("hidden");
-  hiddenForm.classList.remove("hidden");
-  hiddenForm.classList.add("visible");
+    e.currentTarget.classList.remove("inactive");
+    e.currentTarget.classList.add("active");
+
+    let inactive = document.querySelector(".login.inactive");
+    inactive.addEventListener("click", switchLoginForm, true);
+
+    // Enlever le required des formulaires
+    let usernameLogin = document.getElementById("usernameLogin");
+    if (usernameLogin.required) {
+        // Disable Login
+        document.getElementById("usernameLogin").required = false;
+        document.getElementById("passwordLogin").required = false;
+        // Enable Signup
+        document.getElementById("usernameSignup").required = true;
+        document.getElementById("emailSignup").required = true;
+        document.getElementById("passwordSignup").required = true;
+        // Enable password checker
+    } else {
+        // Disable Signup
+        document.getElementById("usernameSignup").required = false;
+        document.getElementById("emailSignup").required = false;
+        document.getElementById("passwordSignup").required = false;
+        // Enable Login
+        document.getElementById("usernameLogin").required = true;
+        document.getElementById("passwordLogin").required = true;
+        // Disable password checker
+    }
+
+    // Remplaçage du formulaire
+    let hiddenForm = document.querySelector(".log-form.hidden");
+    let activeForm = document.querySelector(".log-form.visible");
+    activeForm.classList.remove("visible");
+    activeForm.classList.add("hidden");
+    hiddenForm.classList.remove("hidden");
+    hiddenForm.classList.add("visible");
 
 };
 
 // ToDo: passer en JQueries
 var printErrorLoginForm = function(res) {
-  // ToDo: ne pas filtrer par status code et garder qu'une seule ligne de message d'erreur en printant dedans res.message
+    // ToDo: ne pas filtrer par status code et garder qu'une seule ligne de message d'erreur en printant dedans res.message
     // get("ErrorField")
     // ErrorField.innerHTML = res.message
     // rendre visible
-  console.log(res);
-  if(res.status === 409) {
-    document.getElementById("errorRegister").classList.remove("hidden");
-    document.getElementById("errorRegister").classList.add("visible");
-  } else if(res.status === 400) {
-    document.getElementById("errorLoginID").classList.remove("hidden");
-    document.getElementById("errorLoginID").classList.add("visible");
-    document.getElementById("errorLoginPwd").classList.remove("hidden");
-    document.getElementById("errorLoginPwd").classList.add("visible");
-  }
+    console.log(res);
+    if (res.status === 409) {
+        document.getElementById("errorRegister").classList.remove("hidden");
+        document.getElementById("errorRegister").classList.add("visible");
+    } else if (res.status === 400) {
+        document.getElementById("errorLoginID").classList.remove("hidden");
+        document.getElementById("errorLoginID").classList.add("visible");
+        document.getElementById("errorLoginPwd").classList.remove("hidden");
+        document.getElementById("errorLoginPwd").classList.add("visible");
+    }
 }
 
 let inactive = document.querySelector(".login.inactive");
@@ -65,59 +98,56 @@ inactive.addEventListener("click", switchLoginForm, true);
 
 // Send login-form manually
 let loginform = document.querySelector("#login-form")
-loginform.addEventListener('submit', async (e) => {
-  
-  let body = {
-    usernameLogin: loginform.elements.usernameLogin.value,
-    passwordLogin: loginform.elements.passwordLogin.value
-  }
+loginform.addEventListener('submit', async(e) => {
 
-  e.preventDefault();
-
-  const res = await fetch('/login', {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {'Content-Type': 'application/json'}
-  })
-
-  const data = res.json();
-  data.then(response => {
-    if (response.status === 200) {
-      window.location = response.redirect;
+    let body = {
+        usernameLogin: loginform.elements.usernameLogin.value,
+        passwordLogin: loginform.elements.passwordLogin.value
     }
-    else {
-      // ToDo: print errors
-      printErrorLoginForm(response)
-    }
-  }).catch(error => console.error('Error:', error))
+
+    e.preventDefault();
+
+    const res = await fetch('/login', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    const data = res.json();
+    data.then(response => {
+        if (response.status === 200) {
+            window.location = response.redirect;
+        } else {
+            // ToDo: print errors
+            printErrorLoginForm(response)
+        }
+    }).catch(error => console.error('Error:', error))
 })
 
 // Send register-form manually
 let registerform = document.querySelector("#register-form")
-registerform.addEventListener('submit', async (e) => {
-  
-  let body = {
-    usernameSignup: registerform.elements.usernameSignup.value,
-    emailSignup: registerform.elements.emailSignup.value,
-    passwordSignup: registerform.elements.passwordSignup.value
-  }
+registerform.addEventListener('submit', async(e) => {
 
-  e.preventDefault();
-
-  const res = await fetch('/register', {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {'Content-Type': 'application/json'}
-  })
-
-  const data = res.json();
-  data.then(response => {
-    if (response.status === 201) {
-      window.location = response.redirect;
+    let body = {
+        usernameSignup: registerform.elements.usernameSignup.value,
+        emailSignup: registerform.elements.emailSignup.value,
+        passwordSignup: registerform.elements.passwordSignup.value
     }
-    else {
-      printErrorLoginForm(response)
-    }
-  }).catch(error => console.error('Error:', error))
+
+    e.preventDefault();
+
+    const res = await fetch('/register', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    const data = res.json();
+    data.then(response => {
+        if (response.status === 201) {
+            window.location = response.redirect;
+        } else {
+            printErrorLoginForm(response)
+        }
+    }).catch(error => console.error('Error:', error))
 })
-
