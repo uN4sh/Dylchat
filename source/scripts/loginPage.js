4e3f698a9bdc811ff44cfd2195411af6152cbc44
XAVIER Vincent <vincent.xavier@ens.uvsq.fr>
2022-04-24 17:34:31 +0200

    Affinage du password checker et conseil sur la création d'un mot de passe lors de l'inscription

--- source/scripts/loginPage.js
+++ source/scripts/loginPage.js
@@ -4,7 +4,7 @@ let box2 = document.getElementById("strenght_password_box_2");
 let box3 = document.getElementById("strenght_password_box_3");
 
 let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
-let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}))');
 
 function StrengthChecker(PasswordParameter) {
     if (strongPassword.test(PasswordParameter)) {
